3b26146727dcd077526fdca58221d9f6a0d54660
jponce <jponce@sitetracker.com>
2022-11-10 16:36:41 +0100

    changes

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ app.post('/zoom', async (req, res) => {
   
     const event = req.body.event;
   const accountId = req.body.payload.account_id;
-  console.log("event ${event} accountId ${account_id}");
+  console.log("event ${event} accountId ${accountId}");
   const result = "OK";
-  res.json(item).end()
+  res.json(result).end()
 })
 
 // Delete an item
